f330712b674afe9ecae1f52c525fca288e0d2522
huimin <nancy_4633@163.com>
2020-07-03 17:30:20 +0800

    test

--- Week_02/HeapSort.java
+++ Week_02/HeapSort.java
@@ -61,10 +61,8 @@ public class HeapSort {
     public static void main(String args[]) {
         int arr[] = {12, 11, 13, 5, 6, 7};
         int n = arr.length;
-
         HeapSort ob = new HeapSort();
         ob.sort(arr);
-
         System.out.println("Sorted array is");
         printArray(arr);
     }
