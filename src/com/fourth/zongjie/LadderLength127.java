45fd82b9194d82768bb0891ae273414a80380f7e
huimin <nancy_4633@163.com>
2020-07-15 14:23:08 +0800

    单词最短转换序列

---
+++ src/com/fourth/zongjie/LadderLength127.java
@@ -0,0 +1,195 @@
+package com.fourth.zongjie;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+// 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+// 每次转换只能改变一个字母。转换过程中的中间单词必须是字典中的单词。
+// 说明:
+// 如果不存在这样的转换序列，返回 0。
+// 所有单词具有相同的长度。
+// 所有单词只由小写字母组成。
+// 字典中不存在重复的单词。
+// 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+// 思路：
+// 跟最少基因变化，是同一个解法
+public class LadderLength127 {
+
+    /***
+     * DFS
+     *
+     * 时间复杂度：O(M×N)，其中 MM 是单词的长度 NN 是单词表中单词的总数。找到所有的变换需要对每个单词做 MM 次操作。同时，最坏情况下广度优先搜索也要访问所有的 NN 个单词。
+     * 空间复杂度：O(M×N)，要在 all_combo_dict 字典中记录每个单词的 MM 个通用状态。访问数组的大小是 NN。广搜队列最坏情况下需要存储 NN 个单词。
+     *
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        // Since all words are of same length.
+        int L = beginWord.length();
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        Map<String, List<String>> allComboDict = new HashMap<>();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        // Key is the generic word
+                        // Value is a list of words which have the same intermediate generic word.
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                        List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
+                        transformations.add(word);
+                        allComboDict.put(newWord, transformations);
+                    }
+                });
+
+        // Queue for BFS
+        Queue<Pair<String, Integer>> Q = new LinkedList<>();
+        Q.add(new Pair(beginWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        Map<String, Boolean> visited = new HashMap<>();
+        visited.put(beginWord, true);
+
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < L; i++) {
+
+                // Intermediate words for current word
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+                // Next states are all the words which share the same intermediate state.
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    // If at any point if we find what we are looking for
+                    // i.e. the end word - we can return with the answer.
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    // Otherwise, add it to the BFS Queue. Also mark it visited
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    /***
+     * 双向广度优先搜索
+     *
+     * 时间复杂度：O(M×N)，其中 MM 是单词的长度 NN 是单词表中单词的总数。与单向搜索相同的是，找到所有的变换需要M * N次操作。但是搜索时间会被缩小一半，因为两个搜索会在中间某处相遇。
+     * 空间复杂度：O(M×N)，要在 all_combo_dict 字典中记录每个单词的 MM 个通用状态，这与单向搜索相同。但是因为会在中间相遇，所以双向搜索的搜索空间变小。
+     *
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength11(String beginWord, String endWord, List<String> wordList) {
+
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+
+        // Since all words are of same length.
+        this.L = beginWord.length();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        // Key is the generic word
+                        // Value is a list of words which have the same intermediate generic word.
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                        List<String> transformations =
+                                this.allComboDict.getOrDefault(newWord, new ArrayList<>());
+                        transformations.add(word);
+                        this.allComboDict.put(newWord, transformations);
+                    }
+                });
+
+        // Queues for birdirectional BFS
+        // BFS starting from beginWord
+        Queue<Pair<String, Integer>> Q_begin = new LinkedList<>();
+        // BFS starting from endWord
+        Queue<Pair<String, Integer>> Q_end = new LinkedList<>();
+        Q_begin.add(new Pair(beginWord, 1));
+        Q_end.add(new Pair(endWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        Map<String, Integer> visitedBegin = new HashMap<>();
+        Map<String, Integer> visitedEnd = new HashMap<>();
+        visitedBegin.put(beginWord, 1);
+        visitedEnd.put(endWord, 1);
+
+        while (!Q_begin.isEmpty() && !Q_end.isEmpty()) {
+
+            // One hop from begin word
+            int ans = visitWordNode(Q_begin, visitedBegin, visitedEnd);
+            if (ans > -1) {
+                return ans;
+            }
+
+            // One hop from end word
+            ans = visitWordNode(Q_end, visitedEnd, visitedBegin);
+            if (ans > -1) {
+                return ans;
+            }
+        }
+        return 0;
+    }
+
+    private int L;
+    private Map<String, List<String>> allComboDict;
+
+    LadderLength127() {
+        this.L = 0;
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        this.allComboDict = new HashMap<>();
+    }
+
+    private int visitWordNode(
+            Queue<Pair<String, Integer>> Q,
+            Map<String, Integer> visited,
+            Map<String, Integer> othersVisited) {
+
+        Pair<String, Integer> node = Q.remove();
+        String word = node.getKey();
+        int level = node.getValue();
+
+        for (int i = 0; i < this.L; i++) {
+
+            // Intermediate words for current word
+            String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+            // Next states are all the words which share the same intermediate state.
+            for (String adjacentWord : this.allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                // If at any point if we find what we are looking for
+                // i.e. the end word - we can return with the answer.
+                if (othersVisited.containsKey(adjacentWord)) {
+                    return level + othersVisited.get(adjacentWord);
+                }
+
+                if (!visited.containsKey(adjacentWord)) {
+
+                    // Save the level as the value of the dictionary, to save number of hops.
+                    visited.put(adjacentWord, level + 1);
+                    Q.add(new Pair(adjacentWord, level + 1));
+                }
+            }
+        }
+        return -1;
+    }
+
+}
