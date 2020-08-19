8bd57ca6d59adb2be26dcf85472d4c93640c75e6
huimin <nancy_4633@163.com>
2020-08-19 10:22:48 +0800

    第八周作业更新

--- Week_08/heapify.java
+++ Week_08/heapify.java
@@ -23,7 +23,7 @@ public class heapify {
     public static void heapSort(int[] array) {
         if (array.length == 0) return;
         int length = array.length;
-        for (int i = length / 2 - 1; i >= 0; i -)
+        for (int i = length / 2 - 1; i >= 0; i--)
             heapify(array, length, i);
         for (int i = length - 1; i >= 0; i--) {
             int temp = array[0];
