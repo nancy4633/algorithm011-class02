7e834c69e0000308a93aace6d88fe01b80591ae1
huimin <nancy_4633@163.com>
2020-09-02 15:15:31 +0800

    第二周习题更新

--- src/com/xunlianying2/IsAnagram242.java
+++ src/com/xunlianying2/IsAnagram242.java
@@ -1,10 +1,86 @@
 package com.xunlianying2;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 // 第一遍
 // 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
 public class IsAnagram242 {
+    /**
+     * 数组代替hashmap
+     * 时间复杂度:O() - 99.89%
+     * 空间复杂度:O() - 32.00%
+     * 优点:
+     * 缺点:
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram1(String s, String t) {
+        int[] count1 = new int[26];
+        for (char c : s.toCharArray())
+            count1[c - 97] += 1;
+        for (char c : t.toCharArray())
+            count1[c - 97] -= 1;
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != 0) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 数组实现hash
+     * 时间复杂度:O(n) - 86.56%
+     * 空间复杂度:O(1) - 27.80%
+     * 优点:
+     * 缺点:
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram2(String s, String t) {
+        int lens = s.length(), lent = t.length();
+        if (lens != lent) return false;
+        int[] table = new int[26];
+        for (int i = 0; i < lens; i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < lent; i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) return false;
+        }
+        return true;
+    }
+
+    /**
+     * hashmap - 最慢，这道题不适合用hashmap
+     * 时间复杂度:O() - 18.99%
+     * 空间复杂度:O() - 9.19%
+     * 优点:
+     * 缺点:
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram3(String s, String t) {
+        int lens = s.length(), lent = t.length();
+        if (lens != lent) return false;
+        Map<Character, Integer> charMap1 = new HashMap<>((int) (lens / 0.75F + 1.0F));
+        Map<Character, Integer> charMap2 = new HashMap<>((int) (lent / 0.75F + 1.0F));
+        for (char c : s.toCharArray())
+            charMap1.put(c, charMap1.getOrDefault(c, 0) + 1);
+        for (char c : t.toCharArray())
+            charMap2.put(c, charMap2.getOrDefault(c, 0) + 1);
+        if (charMap1.size() != charMap2.size()) return false;
+        for (char c : s.toCharArray()) {
+            if (!charMap1.get(c).equals(charMap2.getOrDefault(c, 0))) return false;
+        }
+        return true;
+    }
 
     /**
      * 暴力求解：先排序，后对比是否相等。
@@ -16,9 +92,7 @@ public class IsAnagram242 {
      * @param t
      * @return
      */
-    public static boolean isAnagram1(String s, String t) {
-        if (s == null && t == null) return true;
-        if ((s == null) & (t == null) == false) return false;
+    public boolean isAnagram4(String s, String t) {
         if (s.length() != t.length()) return false;
         char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
@@ -29,55 +103,5 @@ public class IsAnagram242 {
         } else {
             return false;
         }
-    }
-
-    /**
-     * hash
-     * 时间复杂度:O(n) - 49.73%
-     * 空间复杂度:O(1) - 28.28%
-     * 优点:
-     * 缺点:
-     *
-     * @param s
-     * @param t
-     * @return
-     */
-    public boolean isAnagram11(String s, String t) {
-        if (s == null && t == null) return true;
-        if ((s == null) & (t == null) == false) return false;
-        if (s.length() != t.length()) return false;
-        int[] sArray = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            sArray[s.charAt(i) - 'a']++;
-            sArray[t.charAt(i) - 'a']--;
-        }
-        for (int i : sArray) {
-            if (i != 0) return false;
-        }
-        return true;
-    }
-
-    /**
-     * 优化的hash
-     * 时间复杂度:O(n) - 64.47%
-     * 空间复杂度:O(1) - 85.17%
-     * 优点:
-     * 缺点:
-     *
-     * @param s
-     * @param t
-     * @return
-     */
-    public boolean isAnagram111(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] table = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            table[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            table[t.charAt(i) - 'a']--;
-            if (table[t.charAt(i) - 'a'] < 0) return false;
-        }
-        return true;
     }
 }
---
+++ src/com/xunlianying2/postorder590.java
@@ -0,0 +1,18 @@
+package com.xunlianying2;
+
+import java.util.List;
+
+// 给定一个 N 叉树，返回其节点值的后序遍历。
+// 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+public class postorder590 {
+
+    /**
+     * @param root
+     * @return
+     */
+    public List<Integer> postorder(Node root) {
+        return null;
+    }
+
+
+}
